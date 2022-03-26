c75df08e93a83b4abc7f05800bbb628299ef7182
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2022-03-26 19:58:55 +0900

    기능개발

--- src/FuncDevelopement.kt
+++ src/FuncDevelopement.kt
@@ -1,16 +1,48 @@
 import java.util.*
 import kotlin.collections.ArrayList
 
-//다시 푸는중
+//품
 class Solution16 {
     fun solution(progresses: IntArray, speeds: IntArray): IntArray {
-        var answer = intArrayOf()
-        return answer
+        var answer = ArrayList<Int>()
+        var num = 1
+        val proQueue : Queue<Int> = LinkedList<Int>()
+        val speedQueue : Queue<Int> = LinkedList<Int>()
+
+        for (i in progresses.indices){
+            proQueue.add(progresses[i])
+            speedQueue.add(speeds[i])
+        }
+
+
+        var isStart = false
+        var count = 0
+        while (true){
+            if (proQueue.peek()+(speedQueue.peek()*num) < 100 && !isStart){
+                num++
+            } else if (proQueue.peek()+(speedQueue.peek()*num) >= 100){
+                count++
+                isStart = true
+                proQueue.poll()
+                speedQueue.poll()
+            } else if (proQueue.peek()+(speedQueue.peek()*num) < 100 && isStart) {
+                answer.add(count)
+                count = 0
+                isStart = false
+            }
+
+            if (proQueue.isEmpty()){
+                answer.add(count)
+                break
+            }
+        }
+
+        return answer.toIntArray()
     }
 
     fun main(args:Array<String>) {
         val prograssAry = intArrayOf(93, 30, 55)
-        val speedAry = intArrayOf(93, 30, 55)
+        val speedAry = intArrayOf(1, 30, 5)
 
         println(solution(prograssAry, speedAry))
     }
