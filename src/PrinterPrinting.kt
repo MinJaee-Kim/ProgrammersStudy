06afb1306a055edc8b9e8e3913276b9159fec3c3
yt90as <yt90as@naver.com>
2022-03-30 17:18:54 +0900

    프린터 알고리즘

--- src/PrinterPrinting.kt
+++ src/PrinterPrinting.kt
@@ -2,32 +2,32 @@ import java.util.*
 
 class Solution17 {
     fun solution(priorities: IntArray, location: Int): Int {
-        var value = 0
-        var number = 0
-        val queuePriorites : Queue<Int> = LinkedList(priorities.asList())
-        var queIter = queuePriorites.iterator()
+        var answer = 0
+        var count = 0
+        val priQueue = PriorityQueue<Int>(Collections.reverseOrder())
 
-        while (queIter.hasNext()){
-            value = queIter.next()
-            if (queuePriorites.maxOrNull() == value){
-                queIter.remove()
-                number++
-                if (priorities[location] === value) {
-                    println("확인")
-                    return number
+        for(i in priorities.indices){
+            priQueue.add(priorities[i])
+        }
+
+        while (!priQueue.isEmpty()){
+            for (i in priorities.indices){
+                if (priorities[i]==priQueue.peek()){
+                    count++
+                    if (i == location){
+                        answer = count
+                    }
+                    priQueue.poll()
                 }
-            }
-            if (!queIter.hasNext()){
-                queIter = queuePriorites.iterator()
             }
         }
 
-        return number
+        return answer
     }
 }
 
 fun main() {
     var priorities: IntArray
-    priorities = intArrayOf(2, 1, 3, 2)
+    priorities = intArrayOf(1, 1, 9, 1, 1, 1)
     println(Solution17().solution(priorities, 0))
 }
