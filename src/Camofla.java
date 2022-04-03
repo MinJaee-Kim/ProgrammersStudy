08ff6826ada0e228d6bf2f4002063e17ceb0f2de
yt90as <yt90as@naver.com>
2022-04-03 20:50:21 +0900

    일단커밋

--- src/Camofla.java
+++ src/Camofla.java
@@ -1,6 +1,22 @@
+import java.util.HashMap;
+
 class Solution19 {
     public int solution(String[][] clothes) {
         int answer = 0;
+        HashMap<String, String> hashMap = new HashMap<>();
+
+        for (int i=0; i<clothes.length; i++){
+            hashMap.put(clothes[i][1], clothes[i][0]);
+        }
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution19 solution19 = new Solution19();
+        String[][] clothes = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"},
+                {"green_turban", "headgear"}};
+
+        System.out.println(solution19.solution(clothes));
     }
 }
