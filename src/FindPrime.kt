250c91d9dcf54c00a0863a3af70b3957466297c4
yt90as <yt90as@naver.com>
2022-04-03 20:22:40 +0900

    더 맵게

--- src/FindPrime.kt
+++ src/FindPrime.kt
@@ -1,12 +1,12 @@
 //덜 품
-class Solution13 {
-    fun solution(numbers: String): Int {
-        var answer = 0
-        return answer
-    }
-}
-
-fun main() {
-    var s = Solution13()
-    println(s.solution("17"))
-}
+//class Solution13 {
+//    fun solution(numbers: String): Int {
+//        var answer = 0
+//        return answer
+//    }
+//}
+//
+//fun main() {
+//    var s = Solution13()
+//    println(s.solution("17"))
+//}
