29da5b7195f7bf367c9cb1ff23df8af0c79aa64e
yt90as <yt90as@naver.com>
2022-04-11 14:57:10 +0900

    일단 커밋

--- src/OpenChat.java
+++ src/OpenChat.java
@@ -3,17 +3,27 @@ class Solution21 {
         String[] answer = {};
 
         for (int i=0; i<record.length; i++){
-            if (record[i].substring(0, 5).equals("Enter") || record[i].substring(0, 6).equals("Change")){
-                for (int j=0; j<record.length; j++){
-                    if (i == j){
-                        continue;
+            if (record[i].substring(0, 5).equals("Enter") ||
+                    record[i].substring(0, 6).equals("Change")){
+                for (int j=0; j<i; j++){
+                    String pre="a";
+                    String fix="b";
+                    if (record[i].substring(0, 5).equals("Enter") ||
+                            record[i].substring(0, 6).equals("Change")){
+                        pre = record[i].substring(record[i].indexOf(" ")+1, record[i].lastIndexOf(" "));
+                        fix = record[j].substring(record[j].indexOf(" ")+1, record[i].lastIndexOf(" "));
                     }
-                    if (record[i].substring(6, 13).equals(record[j].substring(6, 13))){
-                        System.out.println(record[i].substring(6, 13));
-                        System.out.println(record[i].substring(14));
+
+                    if (pre.equals(fix) && record[j].substring(0, 5).equals("Enter") || record[j].substring(0, 6).equals("Change")){
+                        record[j] = record[i];
                     }
                 }
             }
+        }
+
+
+        for (int i=0; i<record.length; i++) {
+            System.out.println(record[i]);
         }
 
         return answer;
