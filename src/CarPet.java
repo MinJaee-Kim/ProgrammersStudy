d7c706069027c1a91192e97b0423e10fa6bfeb64
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2022-04-17 00:10:36 +0900

    담에할거

---
+++ src/CarPet.java
@@ -0,0 +1,6 @@
+class Solution31 {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        return answer;
+    }
+}
---
+++ src/ExpressN.java
@@ -0,0 +1,6 @@
+class Solution32 {
+    public int solution(int N, int number) {
+        int answer = 0;
+        return answer;
+    }
+}
