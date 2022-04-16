f88c83d47169118ebcb4a0f97c6cb995a709bf88
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2022-04-16 23:32:49 +0900

    살짝 수정

--- src/PhonesList.java
+++ src/PhonesList.java
@@ -1,40 +1,7 @@
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 
 class Solution18 {
     public boolean solution(String[] phone_book) {
-//        boolean answer = true;
-//
-//        HashMap<String, Integer> hashMap = new HashMap();
-//
-//        Arrays.sort(phone_book, new Comparator<String>() {
-//            @Override
-//            public int compare(String o1, String o2) {
-//                if (Integer.parseInt(o1) < Integer.parseInt(o2)) {
-//                    return -1;
-//                } else if (Integer.parseInt(o1) > Integer.parseInt(o2)) {
-//                    return 1;
-//                }
-//                return 0;
-//            }
-//        });
-//
-//        for (int i=0; i<phone_book.length; i++){
-//            hashMap.put(phone_book[i], i);
-//        }
-//
-//
-//        for (int i=0; i<phone_book.length; i++){
-//            for (int j=i+1; j< phone_book.length; j++){
-//                if (phone_book[i].equals(phone_book[j].substring(0, phone_book[i].length()))) {
-//                    answer = false;
-//                    break;
-//                }
-//            }
-//        }
-//
-//        return answer;
         boolean answer = true;
 
         HashMap<String, Integer> hashMap = new HashMap();
