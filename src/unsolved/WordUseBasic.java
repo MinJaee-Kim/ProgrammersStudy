9d45e984562537ba8ae3369f8fb2faed574425dd
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2022-05-07 23:23:07 +0900

    문자열 기본

---
+++ src/unsolved/WordUseBasic.java
@@ -0,0 +1,25 @@
+package unsolved;
+
+class Solution63 {
+    public boolean solution(String s) {
+        boolean answer = true;
+
+        if (!(s.length()==4) && !(s.length()==6)) {
+            return false;
+        }
+
+        for (int i=0; i<s.length(); i++){
+            if (s.charAt(i) < 48 || s.charAt(i) > 57){
+                return false;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution63 solution63 = new Solution63();
+
+        System.out.println(solution63.solution("1234567"));
+    }
+}
