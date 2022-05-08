3e4f848994aba456780f7b4e688f0cb4a5c2cc21
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2022-05-08 15:34:30 +0900

    추가

---
+++ src/unsolved/Year2016.java
@@ -0,0 +1,18 @@
+package unsolved;
+
+class Solution72 {
+    public String solution(int a, int b) {
+        String answer = "";
+
+
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Solution72 solution72 = new Solution72();
+
+        System.out.println(solution72.solution(5, 24));
+    }
+}
