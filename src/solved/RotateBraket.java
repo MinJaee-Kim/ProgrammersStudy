d70b4d8985d7b1d7970de6db6e2c800b38e70d00
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2022-05-31 07:52:14 +0900

    괄호 회전하기

---
+++ src/solved/RotateBraket.java
@@ -0,0 +1,64 @@
+package solved;
+
+import java.util.Stack;
+
+class Solution78 {
+    public int solution(String s) {
+        int answer = 0;
+
+        for (int i=0; i<s.length(); i++){
+            if (isRight(s)){
+                answer++;
+            }
+
+            s = s.substring(1)+s.charAt(0);
+        }
+
+        return answer;
+    }
+
+    private boolean isRight(String s) {
+        Stack<Character> stack = new Stack();
+
+        for (int i=0; i<s.length(); i++){
+            if (s.charAt(i)=='[' || s.charAt(i)=='(' || s.charAt(i)=='{'){
+                stack.add(s.charAt(i));
+            } else {
+                if (stack.isEmpty())
+                    return false;
+
+                if (s.charAt(i)==']'){
+                    if (stack.peek()=='['){
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                } else if (s.charAt(i)==')') {
+                    if (stack.peek()=='('){
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                } else if (s.charAt(i)=='}') {
+                    if (stack.peek() == '{'){
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        if (!stack.isEmpty())
+            return false;
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution78 solution78 = new Solution78();
+        String s = "{{{{{{{";
+
+        System.out.println(solution78.solution(s));
+    }
+}
