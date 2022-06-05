f9c642bc7d1eb6e0141af583953d751577994fb5
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2022-06-05 15:31:00 +0900

    테스트용

--- src/unsolved/ExpectLeague.java
+++ src/unsolved/ExpectLeague.java
@@ -3,16 +3,21 @@ package unsolved;
 class Solution57 {
     public int solution(int n, int a, int b) {
         int answer = 0;
+        int first = 0;
+        int second = 0;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+        while (true){
+
+            break;
+        }
 
         return answer;
     }
 
+
     public static void main(String[] args) {
         Solution57 solution57 = new Solution57();
 
-        System.out.println(solution57.solution(8, 4, 7));
+        System.out.println(solution57.solution(16, 1, 15));
     }
 }
