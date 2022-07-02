481176c236afeb30c82b8d380037bfde41e71e54
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2022-07-02 09:00:30 +0900

    평균구하기

---
+++ src/solved/CalcAverage.java
@@ -0,0 +1,15 @@
+package solved;
+
+class Solution112 {
+    public double solution(int[] arr) {
+        double answer = 0;
+
+        for (int i=0; i< arr.length; i++){
+            answer += arr[i];
+        }
+
+        answer/=arr.length;
+
+        return answer;
+    }
+}
