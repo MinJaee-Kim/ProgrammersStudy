e53a9e0d06167ba871b4eaba66f2ed50cd1389b2
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2022-08-02 10:18:04 +0900

    야근지수

--- src/unsolved/BestSet.java
+++ src/unsolved/BestSet.java
@@ -1,20 +1,25 @@
 package unsolved;
 
+import java.util.ArrayList;
+
 class Solution99 {
+    int max = 0;
     public int[] solution(int n, int s) {
         int[] answer = {};
 
+        dfs(new ArrayList<Integer>(), n, 0, 0, s);
+        System.out.println(max);
 
         return answer;
     }
 
-    private void dfs(int n, int result) {
+    private void dfs(ArrayList<Integer> numList, int n, int m, int add, int result) {
 
     }
 
     public static void main(String[] args) {
         Solution99 solution99 = new Solution99();
 
-        System.out.println(solution99.solution(4, 9));
+        System.out.println(solution99.solution(2, 9));
     }
 }
---
+++ src/unsolved/NightShiftVal.java
@@ -0,0 +1,34 @@
+package unsolved;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class Solution129 {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        int num = 0;
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue(Collections.reverseOrder());
+
+        for (Integer i:works) {
+            priorityQueue.add(i);
+        }
+
+        for (int i=0; i<n; i++) {
+            num = priorityQueue.poll()-1;
+            priorityQueue.add(Math.max(num, 0));
+        }
+
+        while (!priorityQueue.isEmpty()){
+            answer+=Math.pow(priorityQueue.poll(), 2);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution129 solution129 = new Solution129();
+        int[] works = {1, 1};
+
+        System.out.println(solution129.solution(3, works));
+    }
+}
