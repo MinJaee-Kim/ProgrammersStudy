1ab7550b50952f5f03adce0396f9a4933a2c87c1
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2024-04-30 04:49:17 +0900

    쉬운거

---
+++ src/unsolved/PlusNumber.java
@@ -0,0 +1,18 @@
+package unsolved;
+
+public class PlusNumber {
+    private int solution(int n) {
+        int answer = 0;
+
+        for (String s: String.valueOf(n).split("")) {
+            answer+=Integer.parseInt(s);
+        }
+
+        return answer;
+    }
+    private static void main(String[] args) {
+        PlusNumber plusNumber = new PlusNumber();
+
+        System.out.println(plusNumber.solution(123));
+    }
+}
