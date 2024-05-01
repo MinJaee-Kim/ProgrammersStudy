7847b0d27db299d281863d1651f3af11bc0be0c7
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2024-05-01 20:52:04 +0900

    쉬운 홀짝

---
+++ src/unsolved/OddAndEven.java
@@ -0,0 +1,13 @@
+package unsolved;
+
+public class OddAndEven {
+    public String solution(int num) {
+        if(num % 2 == 0) return "Even";
+        else return "Odd";
+    }
+    public static void main(String[] args) {
+        OddAndEven o = new OddAndEven();
+
+        System.out.println(o.solution(1));
+    }
+}
