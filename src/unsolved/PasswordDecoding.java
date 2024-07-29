c90f96b9eee7cc2dd2921f3848c580dbf21c8951
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2024-07-29 23:00:15 +0900

    한줄충2

--- src/unsolved/PasswordDecoding.java
+++ src/unsolved/PasswordDecoding.java
@@ -1,11 +1,16 @@
 package unsolved;
 
+// import java.util.stream.IntStream;
 import java.util.stream.Collectors;
 import java.util.concurrent.atomic.AtomicInteger;
 
 class PasswordDecoding {
     public String solution(String cipher, int code) {
         AtomicInteger index = new AtomicInteger();
+        // return IntStream.range(0, cipher.length())
+        //         .filter(value -> value % code == code - 1)
+        //         .mapToObj(c -> String.valueOf(cipher.charAt(c)))
+        //         .collect(Collectors.joining());
         
         return cipher.chars().mapToObj(c->(char) c).filter(e->index.getAndIncrement()%code==code-1).map(String::valueOf).collect(Collectors.joining());
     }
