a02c18f708b121c2a37b6b02fd13d6bb1e7f9881
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2024-07-31 23:17:12 +0900

    풀기전

---
+++ src/unsolved/RoughlyKeys.java
@@ -0,0 +1,14 @@
+package unsolved;
+
+class RoughlyKeys {
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        RoughlyKeys r = new RoughlyKeys();
+
+        System.out.println(r.solution(new String[]{"ABACD", "BCEFD"}, new String[]{"ABCD","AABB"}));
+    }
+}
