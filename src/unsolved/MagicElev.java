68a73417f0b44c07c096f6776562f8b6f84ba935
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2024-08-13 18:58:00 +0900

    마법의 엘리베이터 푸는중

--- src/unsolved/MagicElev.java
+++ src/unsolved/MagicElev.java
@@ -1,4 +1,13 @@
 package unsolved;
 
 public class MagicElev {
+    public int solution(int storey) {
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+        MagicElev m = new MagicElev();
+
+        System.out.println(m.solution(16));
+    }
 }
