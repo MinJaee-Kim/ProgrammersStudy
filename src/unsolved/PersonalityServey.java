19121dd313852a63a6d11103ef2adf1619b72a33
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2024-08-21 02:29:51 +0900

    두개 킵

---
+++ src/unsolved/PersonalityServey.java
@@ -0,0 +1,16 @@
+package unsolved;
+
+class PersonalityServey {
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        PersonalityServey p = new PersonalityServey();
+        String[] survey = {"AN", "CF", "MJ", "RT", "NA"};
+        int[] choices = {5, 3, 2, 7, 5};
+
+        System.out.println(p.solution(survey, choices));
+    }
+}
---
+++ src/unsolved/SkippingLadder.java
@@ -0,0 +1,15 @@
+package unsolved;
+
+class SkippingLadder {
+    public int solution(int[] stones, int k) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        SkippingLadder s = new SkippingLadder();
+        int[] stones = {2, 4, 5, 3, 2, 1, 4, 2, 5, 1};
+
+        System.out.println(s.solution(stones, 3));
+    }
+}
