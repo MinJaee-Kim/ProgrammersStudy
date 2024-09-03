d7c4a2320c1def929219f5e86e7885a03ac68125
MinJaee-Kim <75728238+MinJaee-Kim@users.noreply.github.com>
2024-09-03 11:02:14 +0900

    O(n)제거 실패

--- src/unsolved/LifeBoat.java
+++ src/unsolved/LifeBoat.java
@@ -1,37 +1,42 @@
 package unsolved;
 
-import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Deque;
+import java.util.Stack;
 
 class Solution47 {
     public int solution(int[] people, int limit) {
         int answer = 0;
         int index = 0;
-        Deque<Integer> deque = new ArrayDeque<Integer>();
+        int stackIndex = 0;
+        int removeCount = 0;
+        Stack<Integer> stack = new Stack<>();
         Integer[] convertPeople = Arrays.stream(people).boxed().toArray(Integer[]::new);
         Arrays.sort(convertPeople, Collections.reverseOrder());
         
-        while(people.length>index||deque.size()!=0){
-            if(people.length>index) deque.add(convertPeople[index]);
+        while(people.length>index||people.length>removeCount){
+            if(people.length>index) stack.add(convertPeople[index]);
             
             index++;
-            if(deque.size()>=2&&limit>=deque.peekFirst()+deque.peekLast()){
-                deque.poll();
-                deque.pop();
+            if(stackIndex<stack.size()&&stack.get(stackIndex)+stack.get(stack.size()-1)<=limit){
+                stack.pop();
+                stackIndex++;
+                removeCount++;
+                removeCount++;
                 answer++;
             }else if(index>people.length){
-                deque.poll();
+                removeCount++;
+                stackIndex++;
                 answer++;
             }
         }
+        
         return answer;
     }
 
     public static void main(String[] args) {
         Solution47 solution47 = new Solution47();
-        int[] people = {40, 50, 60, 90};
+        int[] people = {70, 50, 80, 50};
         int limit = 100;
 
         System.out.println(solution47.solution(people, limit));
